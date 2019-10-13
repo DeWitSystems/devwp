18a2687e537e82875925d7f0f9e3f72eb17e5a3b
northphex <54204550+northphex@users.noreply.github.com>
2019-10-13 15:28:48 +0200

    Add files via upload

---
+++ page_full_width.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Full Widht Page
+ *
+ * This is the template that displays without sidebars
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DevWP
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area col-md-12">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer();
